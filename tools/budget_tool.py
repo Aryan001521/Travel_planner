f643001d90f61b3facb1a0c71ef6876bf10a1121
aryan sharma <127038461+Aryan001521@users.noreply.github.com>
2026-05-19 18:41:12 +0530

    Update budget_tool.py

--- tools/budget_tool.py
+++ tools/budget_tool.py
@@ -1,13 +1,37 @@
- #BUDGET CALCULATION
+# BUDGET CALCULATION
 def calculate_total_cost(flight, hotel, days):
+
     total_cost = 0
-    # Flight Cost
-    if flight and isinstance(flight, dict):
-        total_cost += flight['price']
-    # Hotel Cost
-    if hotel and isinstance(hotel, dict):
-        total_cost += (hotel['price_per_night'] * days)
-    # Extra expenses
-    food_and_transport = 2000
-    total_cost += food_and_transport
+
+    valid_trip = False
+
+    # FLIGHT COST
+    if (
+        flight
+        and isinstance(flight, dict)
+        and flight.get("price")
+    ):
+
+        total_cost += flight["price"]
+        valid_trip = True
+
+    # HOTEL COST
+    if (
+        hotel
+        and isinstance(hotel, dict)
+        and hotel.get("price_per_night")
+    ):
+
+        total_cost += (
+            hotel["price_per_night"] * days
+        )
+
+        valid_trip = True
+
+    # EXTRA EXPENSES
+    if valid_trip:
+
+        food_and_transport = 2000
+        total_cost += food_and_transport
+
     return total_cost
